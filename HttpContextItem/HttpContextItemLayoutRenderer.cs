b9d45043613bfcf4a6dd00f871b73cc24bffe3c8
Peter Tucker <peter.tucker@guaranteedrate.com>
2015-07-10 09:10:35 -0500

    Initial commit.

---
+++ HttpContextItem/HttpContextItemLayoutRenderer.cs
@@ -0,0 +1,21 @@
+﻿namespace HttpContextItem
+{
+    using System.Text;
+    using System.Web;
+    using NLog;
+    using NLog.Config;
+    using NLog.LayoutRenderers;
+
+    [LayoutRenderer("http-context-item")]
+    public class HttpContextItemLayoutRenderer : LayoutRenderer
+    {
+        [RequiredParameter]
+        public string ItemName { get; set; }
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var itemValue = HttpContext.Current.Items[ItemName];
+            builder.Append(itemValue);
+        }
+    }
+}
